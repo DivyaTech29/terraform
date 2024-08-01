1309768dc362aa7be439aabe12fe9686f9fdf45d
DivyaTech29 <nadavapallisaidivya@gmail.com>
2024-08-01 10:27:25 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     region = "us-west-2"
-    acces-key = var.aws_access_key
-    secret-key = var.aws_secret_key
+    access_key = var.aws_access_key
+    secret_key = var.aws_secret_key
 }
