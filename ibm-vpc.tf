1be62acbd66b5ef05e5e3359370992be1d8d477a
DivyaTech29 <nadavapallisaidivya@gmail.com>
2024-08-02 09:47:22 +0530

    Update ibm-vpc.tf

--- ibm-vpc.tf
+++ ibm-vpc.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "ibm-vpc" {
+resource "aws_vpc" "ibm_vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
@@ -6,24 +6,27 @@ resource "aws_vpc" "ibm-vpc" {
     Name = "IBM-VPC"
   }
 }
+
 resource "aws_subnet" "ibm_web_sn" {
-  vpc_id     = aws_vpc.ibm-vpc.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.ibm_vpc.id
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "us-west-2a"
 
   tags = {
     Name = "ibm_web_subnet"
   }
 }
+
 resource "aws_subnet" "ibm_db_sn" {
-  vpc_id     = aws_vpc.ibm-vpc.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.ibm_vpc.id
+  cidr_block        = "10.0.2.0/24"
   availability_zone = "us-west-2b"
 
   tags = {
-    Name = "ibm_databse_subnet"
+    Name = "ibm_database_subnet"
   }
 }
+
 resource "aws_internet_gateway" "ibm_igw" {
   vpc_id = aws_vpc.ibm_vpc.id
 
@@ -31,6 +34,7 @@ resource "aws_internet_gateway" "ibm_igw" {
     Name = "ibm_igw"
   }
 }
+
 resource "aws_route_table" "ibm_web_rt" {
   vpc_id = aws_vpc.ibm_vpc.id
 
@@ -43,6 +47,7 @@ resource "aws_route_table" "ibm_web_rt" {
     Name = "ibm_web_routetable"
   }
 }
+
 resource "aws_route_table" "ibm_db_rt" {
   vpc_id = aws_vpc.ibm_vpc.id
 
@@ -50,14 +55,17 @@ resource "aws_route_table" "ibm_db_rt" {
     Name = "ibm_db_routetable"
   }
 }
+
 resource "aws_route_table_association" "ibm_web_rt_associate" {
   subnet_id      = aws_subnet.ibm_web_sn.id
   route_table_id = aws_route_table.ibm_web_rt.id
 }
+
 resource "aws_route_table_association" "ibm_db_rt_associate" {
   subnet_id      = aws_subnet.ibm_db_sn.id
   route_table_id = aws_route_table.ibm_db_rt.id
 }
+
 resource "aws_network_acl" "ibm_web_nacl" {
   vpc_id = aws_vpc.ibm_vpc.id
 
@@ -83,6 +91,7 @@ resource "aws_network_acl" "ibm_web_nacl" {
     Name = "ibm_web_nacl"
   }
 }
+
 resource "aws_network_acl" "ibm_db_nacl" {
   vpc_id = aws_vpc.ibm_vpc.id
 
@@ -108,11 +117,13 @@ resource "aws_network_acl" "ibm_db_nacl" {
     Name = "ibm_db_nacl"
   }
 }
-resource "aws_network_acl_association" "ibm_web_nacl_associate"{
-  subnet_id = aws_subnet.ibm_web_sn.id
-  nacl_id = aws_network_acl.ibm_web_nacl.id
+
+resource "aws_network_acl_association" "ibm_web_nacl_associate" {
+  subnet_id      = aws_subnet.ibm_web_sn.id
+  network_acl_id = aws_network_acl.ibm_web_nacl.id
 }
-resource "aws_network_acl_association" "ibm_db_nacl_associate"{
-  subnet_id = aws_subnet.ibm_db_sn.id
-  nacl_id = aws_network_acl.ibm_db_nacl.id
+
+resource "aws_network_acl_association" "ibm_db_nacl_associate" {
+  subnet_id      = aws_subnet.ibm_db_sn.id
+  network_acl_id = aws_network_acl.ibm_db_nacl.id
 }
